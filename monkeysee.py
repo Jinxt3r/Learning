85ff020542a62f574e93126c5f30f86d90979de7
Jinxt3r <45838825+Jinxt3r@users.noreply.github.com>
2018-12-12 22:55:32 -0500

    Add files via upload

---
+++ monkeysee.py
@@ -0,0 +1,11 @@
+def info():
+    #ask for first firstname
+    firstname = input('What is your first name? ')
+    #ask for last name
+    lastname = input('What is your last name? ')
+    #ask for age
+    age = input('What is your age? ')
+    #print information
+    print(info)
+
+info()
