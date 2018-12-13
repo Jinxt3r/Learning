d69599f53cba33330b632cf3f57b69a839cd6017
Jinxt3r <45838825+Jinxt3r@users.noreply.github.com>
2018-12-12 22:57:46 -0500

    Update monkeysee.py

--- monkeysee.py
+++ monkeysee.py
@@ -8,4 +8,12 @@ def info():
     #print information
     print(info)
 
+def location():
+    #ask for city
+    city = input('What city do you live in? ')
+    #ask for state
+    state = input('What state do you live in? ')
+    #ask for country
+    country = input('What country do you live in? ')
+    
 info()
